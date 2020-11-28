2de9bf23b580f1bf15c3364a832d5f8f7b506b78
Maciej Kłak <klak.maciej@gmail.com>
2020-11-28 09:02:58 +0100

    Date time test

--- src/Misc/Factory/DateTimeFactory.php
+++ src/Misc/Factory/DateTimeFactory.php
@@ -6,7 +6,6 @@ namespace Gilek\Ewus\Misc\Factory;
 use DateTimeImmutable;
 use Gilek\Ewus\Misc\Exception\InvalidDateException;
 
-// TODO find better place
 class DateTimeFactory
 {
     /**
@@ -19,8 +18,8 @@ class DateTimeFactory
     public function createDateTime(string $dateTime): DateTimeImmutable
     {
         $dateObject = DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.vO', $dateTime);
-        if ($dateObject === false) {
-            throw new InvalidDateException('Can\'t create date from "%s".', $dateTime);
+        if ($dateObject === false || $this->hasDateTimeReportedErrors()) {
+            throw new InvalidDateException(sprintf('Can\'t create date from "%s".', $dateTime));
         }
 
         return $dateObject;
@@ -36,10 +35,20 @@ class DateTimeFactory
     public function createDate(string $date): DateTimeImmutable
     {
         $dateObject = DateTimeImmutable::createFromFormat('!Y-m-dO', $date);
-        if ($dateObject === false) {
-            throw new InvalidDateException('Can\'t create date from "%s".', $date);
+        if ($dateObject === false || $this->hasDateTimeReportedErrors()) {
+            throw new InvalidDateException(sprintf('Can\'t create date from "%s".', $date));
         }
 
         return $dateObject;
+    }
+
+    /**
+     * @return bool
+     */
+    private function hasDateTimeReportedErrors(): bool
+    {
+        $errors = DateTimeImmutable::getLastErrors();
+
+        return $errors['warning_count'] > 0 || $errors['error_count'] > 0;
     }
 }
---
+++ tests/Unit/Misc/Factory/DateTimeFactoryTest.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+
+namespace Gilek\Ewus\Test\Unit\Misc\Factory;
+
+use Generator;
+use Gilek\Ewus\Misc\Exception\InvalidDateException;
+use Gilek\Ewus\Misc\Factory\DateTimeFactory;
+use PHPUnit\Framework\TestCase;
+
+final class DateTimeFactoryTest extends TestCase
+{
+    /** @var DateTimeFactory */
+    private $sut;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->sut = new DateTimeFactory();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_create_date_time(): void
+    {
+        $dateTime = $this->sut->createDateTime('2020-11-20T13:44:59.406+01:00');
+
+        $this->assertSame('2020-11-20 13:44:59.406000 GMT+0100', $dateTime->format('Y-m-d H:i:s.u T'));
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidDateTimeDataProvider()
+     *
+     * @param string $invalidDateTime
+     */
+    public function is_should_throw_on_invalid_date_time(string $invalidDateTime): void
+    {
+        $this->expectException(InvalidDateException::class);
+        $this->expectExceptionMessage('Can\'t create date from "'. $invalidDateTime .'".');
+        $this->sut->createDateTime($invalidDateTime);
+    }
+
+    /**
+     * @return Generator
+     */
+    public function invalidDateTimeDataProvider(): Generator
+    {
+        yield ['2020-02-30T13:44:59.406+01:00'];
+        yield ['2020-02-28T13:61:59.406+01:00'];
+        yield ['2020-01-30T13:44:59.406'];
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_create_date(): void
+    {
+        $dateTime = $this->sut->createDate('2020-11-20+01:00');
+
+        $this->assertSame('2020-11-20 00:00:00.000000 GMT+0100', $dateTime->format('Y-m-d H:i:s.u T'));
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidDateTimeDataProvider()
+     *
+     * @param string $invalidDate
+     */
+    public function is_should_throw_on_invalid_date(string $invalidDate): void
+    {
+        $this->expectException(InvalidDateException::class);
+        $this->expectExceptionMessage('Can\'t create date from "'. $invalidDate .'".');
+        $this->sut->createDate($invalidDate);
+    }
+
+    /**
+     * @return Generator
+     */
+    public function invalidDateDataProvider(): Generator
+    {
+        yield ['2020-02-30+01:00'];
+        yield ['2020-01-30'];
+    }
+}
